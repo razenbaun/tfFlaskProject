f4e6afc327bc641b48ef2e306a1808f19a0bb491
Alexander Petrachkov <petrachkov04@list.ru>
2024-10-21 12:23:45 +0300

    add keras

--- model/fit_neuron_weather.py
+++
@@ -1,6 +0,0 @@
-from keras.datasets import fashion_mnist
-from keras import utils
-from keras.models import Sequential
-from tensorflow.keras.layers import Dense, Dropout
-import matplotlib.pyplot as plt
-import random
